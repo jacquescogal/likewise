8a8607fdcf62fcc2a2a8b9567e26756dafb7f726
jasperchia <jasper.dev0@gmail.com>
2022-09-27 13:23:48 +0800

    Implement Basic GoogleMap view on Map Page
    * Imported @react-google-maps/api
    * Added functionality to show a map at a random location
    
    References:
    https://www.npmjs.com/package/@react-google-maps/api
    https://react-google-maps-api-docs.netlify.app/
    https://react-google-maps-api-docs.netlify.app/#!/useLoadScript
    https://www.youtube.com/watch?v=9e-5QHpadi0

--- src/view/pages/Map.js
+++ src/view/pages/Map.js
@@ -1,6 +1,30 @@
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+
 const Map = () => {
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  })
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>
+  }
+
   return(
-    <h1>Map</h1>
+    isLoaded && <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        />
   )
 }
 
