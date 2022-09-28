0fed65acbac32195af66d504d25326b9489fec16
jasperchia <jasper.dev0@gmail.com>
2022-09-28 20:11:10 +0800

    Add Autocomplete component in EventRoomCreate.js
    Basic Autocomplete component taken from the material-ui documentation. It is able to detect the input in the box and suggest location predictions based on Google Maps api call.
    
    TODO:
    * Connect it to a location variable to be sent into the Chat Room
    * Reformat it to a TextField component if possible
    
    References:
    https://mui.com/material-ui/react-autocomplete/#google-maps-place

--- src/view/components/EventRoomCreate.js
+++ src/view/components/EventRoomCreate.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -21,11 +20,33 @@ import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { MobileDateTimePicker } from '@mui/x-date-pickers';
 import { DateTimePicker } from '@mui/x-date-pickers';
 
-import { useState } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { serverTimestamp } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 
-export default function EventRoomCreate({openCreate,setOpenCreate,createChatRoom}) {
+import Box from '@mui/material/Box';
+import Autocomplete from '@mui/material/Autocomplete';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import parse from 'autosuggest-highlight/parse';
+import throttle from 'lodash/throttle';
+
+function loadScript(src, position, id) {
+  if (!position) {
+    return;
+  }
+
+  const script = document.createElement('script');
+  script.setAttribute('async', '');
+  script.setAttribute('id', id);
+  script.src = src;
+  position.appendChild(script);
+}
+
+const autocompleteService = { current: null };
+
+const EventRoomCreate = ({openCreate,setOpenCreate,createChatRoom}) => {
 
     const [cap,setCap]=useState(1)
     const [roomName,setRoomName]=useState('')
@@ -58,6 +79,68 @@ export default function EventRoomCreate({openCreate,setOpenCreate,createChatRoom
     }
   };
 
+  const [value, setValue] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState([]);
+  const loaded = useRef(false);
+
+  if (typeof window !== 'undefined' && !loaded.current) {
+    if (!document.querySelector('#google-maps')) {
+      loadScript(
+        `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`,
+        document.querySelector('head'),
+        'google-maps',
+      );
+    }
+
+    loaded.current = true;
+  }
+
+  const fetch = useMemo(
+    () =>
+      throttle((request, callback) => {
+        autocompleteService.current.getPlacePredictions(request, callback);
+      }, 200),
+    [],
+  );
+
+  useEffect(() => {
+    let active = true;
+
+    if (!autocompleteService.current && window.google) {
+      autocompleteService.current =
+        new window.google.maps.places.AutocompleteService();
+    }
+    if (!autocompleteService.current) {
+      return undefined;
+    }
+
+    if (inputValue === '') {
+      setOptions(value ? [value] : []);
+      return undefined;
+    }
+
+    fetch({ input: inputValue }, (results) => {
+      if (active) {
+        let newOptions = [];
+
+        if (value) {
+          newOptions = [value];
+        }
+
+        if (results) {
+          newOptions = [...newOptions, ...results];
+        }
+
+        setOptions(newOptions);
+      }
+    });
+
+    return () => {
+      active = false;
+    };
+  }, [value, inputValue, fetch]);
+
   return (
     <div>
       <Dialog open={openCreate} onClose={handleClose}>
@@ -84,7 +167,68 @@ export default function EventRoomCreate({openCreate,setOpenCreate,createChatRoom
               variant="standard"
               onChange={e=>setLocation(e.target.value)}
             />
-            
+            <Autocomplete
+              id="google-map-demo"
+              //sx={{ width: 300 }}
+              getOptionLabel={(option) =>
+                typeof option === 'string' ? option : option.description
+              }
+              filterOptions={(x) => x}
+              options={options}
+              autoComplete
+              includeInputInList
+              filterSelectedOptions
+              value={value}
+              onChange={(event, newValue) => {
+                setOptions(newValue ? [newValue, ...options] : options);
+                setValue(newValue);
+                setLocation(newValue);
+              }}
+              onInputChange={(event, newInputValue) => {
+                setInputValue(newInputValue);
+              }}
+              renderInput={(params) => (
+                <TextField {...params} label="Add a location" fullWidth />
+              )}
+              renderOption={(props, option) => {
+                const matches = option.structured_formatting.main_text_matched_substrings;
+                const parts = parse(
+                  option.structured_formatting.main_text,
+                  matches.map((match) => [match.offset, match.offset + match.length]),
+                );
+
+                return (
+                  <li {...props}>
+                    <Grid container alignItems="center">
+                      <Grid item>
+                        <Box
+                          component={LocationOnIcon}
+                          sx={{ color: 'text.secondary', mr: 2 }}
+                        />
+                      </Grid>
+                      <Grid item xs>
+                        {parts.map((part, index) => (
+                          <span
+                            key={index}
+                            style={{
+                              fontWeight: part.highlight ? 700 : 400,
+                            }}
+                          >
+                            {part.text}
+                          </span>
+                        ))}
+
+                        <Typography variant="body2" color="text.secondary">
+                          {option.structured_formatting.secondary_text}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </li>
+                );
+              }}
+            />
+
+
             {/* DateTime Picker */}
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <MobileDateTimePicker
@@ -125,6 +269,9 @@ export default function EventRoomCreate({openCreate,setOpenCreate,createChatRoom
           <Button onClick={handleCreate}>Create</Button>
         </DialogActions>
       </Dialog>
+      {/* <h1>{location}</h1> */}
     </div>
   );
 }
+
+export default EventRoomCreate
